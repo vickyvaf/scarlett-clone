b31b9a36da1cea421fd09f90173008f82b6ab425
vickyvaf <vickyadi243@gmail.com>
2022-09-07 21:32:41 +0700

    add footer

---
+++ year.js
@@ -0,0 +1,8 @@
+const year = document.getElementById("year");
+year.innerHTML = new Date().getFullYear(year);
+
+
+const footer = document.getElementById("footer");
+footer.style.position = "absolute";
+footer.style.bottom = "-800px";
+
